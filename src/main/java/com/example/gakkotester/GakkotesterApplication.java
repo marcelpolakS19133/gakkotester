3918b702a9c4c0fb78d27bb17e0c6e82e2008c50
polmo <polmo@MAREK>
2020-06-29 19:45:05 +0200

    Initial commit

---
+++ src/main/java/com/example/gakkotester/GakkotesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.gakkotester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GakkotesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GakkotesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gakkotester/GakkotesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gakkotester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GakkotesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
