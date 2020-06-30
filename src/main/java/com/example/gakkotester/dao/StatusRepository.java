4793af4da9febf40bad987f41577273087e845b3
polmo <polmo@MAREK>
2020-06-30 19:55:36 +0200

    synchronous code rewrite to async

--- src/main/java/com/example/gakkotester/dao/StatusRepository.java
+++ src/main/java/com/example/gakkotester/dao/StatusRepository.java
@@ -3,12 +3,16 @@ package com.example.gakkotester.dao;
 import com.example.gakkotester.models.Status;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.scheduling.annotation.Async;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Date;
+import java.util.concurrent.Future;
 
 public interface StatusRepository extends CrudRepository<Status, Long> {
-    public Long countStatusesByDateTimeBetweenAndStatus(Date dateTime, Date dateTime2, String status);
-    public Long countStatusesByStatus(String status);
+    @Async
+    Future<Long> countStatusesByDateTimeBetweenAndStatus(Date dateTime, Date dateTime2, String status);
+    @Async
+    Future<Long> countStatusesByStatus(String status);
 }
